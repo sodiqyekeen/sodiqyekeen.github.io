dfd591bfd3dd500938ef438dbc15f4912caebcc2
Bro-Zach <olatechlove@gmail.com>
2021-01-14 23:35:38 +0100

    website setup

---
+++ SodiqYekeen.Site/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SodiqYekeen.Site.Models
+{
+    public class Comment
+    {
+        public Guid CommentId { get; set; }
+        public string User { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
